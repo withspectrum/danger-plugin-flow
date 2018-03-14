96fb421aa1df40b3612870d78c7365668749ce54
Maximilian Stoiber <contact@mxstbr.com>
2018-03-14 14:15:34 +0100

    feat(core): cleanup posted messages, note changed/new status of file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ declare var require: (string) => any
 // tslint:disable-next-line
 const mm = require("micromatch")
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
-import {DangerDSLType} from "../node_modules/danger/distribution/dsl/DangerDSL"
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
 declare var danger: DangerDSLType
 export declare function message(message: string): void
 export declare function warn(message: string): void
@@ -12,9 +12,9 @@ export declare function markdown(message: string): void
 const ENDS_WITH_JS = /jsx?$/i
 
 export interface Options {
-  blacklist?: string[];
-  warn?: boolean;
-  modified?: boolean;
+  blacklist?: string[]
+  warn?: boolean
+  modified?: boolean
 }
 
 /**
@@ -24,7 +24,8 @@ export default async function flow(options: Options = {}) {
   const blacklist = options && options.blacklist ? options.blacklist : []
   const includeModifiedFiles = options.modified === false ? false : true
   const method = options.warn ? warn : fail
-  const jsFiles = danger.git.created_files.concat(includeModifiedFiles ? danger.git.modified_files : [])
+  const jsFiles = danger.git.created_files
+    .concat(includeModifiedFiles ? danger.git.modified_files : [])
     .filter(path => ENDS_WITH_JS.test(path))
   const unignoredFiles: string[] = mm.not(jsFiles, blacklist)
   const files = await Promise.all(
@@ -32,11 +33,15 @@ export default async function flow(options: Options = {}) {
       danger.github.utils.fileContents(path).then(content => ({
         path,
         content,
-      })),
-    ),
+      }))
+    )
   )
 
   const unflowedFiles = files.filter(({ content }) => !/@flow/.test(content)).map(({ path }) => path)
 
-  method(`These new files do not have Flow enabled:\n - ${unflowedFiles.join("\n - ")}`)
+  method(
+    `These new files do not have Flow enabled:\n - ${unflowedFiles
+      .map(path => `${path} (${danger.git.modified_files.indexOf(path) > -1 ? "modified" : "new"})`)
+      .join("\n - ")}`
+  )
 }
